ddb6a75aab7d48d3992384b00486887e58ec9416
mariam <mariamj.uob@gmail.com>
2024-11-30 17:30:04 +0300

    api

---
+++ index1.php
@@ -0,0 +1,8 @@
+<?php
+// Fetching data from the API
+$url = "https://data.gov.bh/api/explore/v2.1/catalog/datasets/01-statistics-of-students-nationalities_updated/records?where=colleges%20like%20%22IT%22%20AND%20the_programs%20like%20%22bachelor%22&limit=100";
+$response = file_get_contents($url);
+
+if ($response === false) {
+    die('Error fetching data. The API request failed.');
+}
